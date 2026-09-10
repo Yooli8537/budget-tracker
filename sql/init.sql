278b0b39dd248368b55a1178e6d455a263931d34
Yooli8537 <Yooli8537@gmx.ch>
2026-09-10 15:33:50 +0200

    Added initialization SQL

---
+++ sql/init.sql
@@ -0,0 +1,8 @@
+-- Is created once upon launching the app for the first time.
+CREATE TABLE IF NOT EXISTS users (
+    userID INT PRIMARY KEY AUTOINCREMENT,
+    isAdmin BOOLEAN NOT NULL,
+    username TEXT NOT NULL UNIQUE,
+    salt TEXT NOT NULL,
+    hashword TEXT NOT NULL
+);
