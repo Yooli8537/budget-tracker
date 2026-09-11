f0c5de11dfd6e7cd5ec931587444f51429f57396
Yooli8537 <Yooli8537@gmx.ch>
2026-09-11 08:24:15 +0200

    Fixed global variables

--- server/global.js
+++ server/global.js
@@ -1,12 +1,14 @@
 // Global server variables
 const path = require("path");
 
-const DATA_FOLDER = path.join(__dirname, "data");
-const LOGS_FOLDER = path.join(__dirname, "logs");
+const ROOT_FOLDER = path.join(__dirname, "../");
+const DATA_FOLDER = path.join(ROOT_FOLDER, "data");
+const LOGS_FOLDER = path.join(ROOT_FOLDER, "logs");
 const PRIVATE_FOLDER = path.join(DATA_FOLDER, "private");
 const USERS_FOLDER = path.join(DATA_FOLDER, "users");
 
 const GLOBAL = {
+  ROOT_FOLDER: ROOT_FOLDER,
   DATA_FOLDER: DATA_FOLDER,
   LOGS_FOLDER: LOGS_FOLDER,
   PRIVATE_FOLDER: PRIVATE_FOLDER,
