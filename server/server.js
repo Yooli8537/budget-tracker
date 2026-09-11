83a8b125b1855d108c4a5fd447e2a65132ef1b69
Yooli8537 <Yooli8537@gmx.ch>
2026-09-11 08:25:24 +0200

    Missing data folders are created automatically

--- server/server.js
+++ server/server.js
@@ -1,16 +1,41 @@
 const express = require("express");
-const path = require("path");
 const fs = require("fs");
+
+const logger = require("./logger");
+const error = require("./error");
+const GLOBAL = require("./global");
 
 const app = express();
 const port = 8561; // If you change the port, make sure to also change it in the vite.config.js
 
 let dataFolderAdjusted = false;
 
-const dataFolder = path.join(__dirname, "data");
-const logsFolder = path.join(__dirname, "logs");
-const privateFolder = path.join(dataFolder, "private");
-const usersFolder = path.join(dataFolder, "users");
+const dataFolders = [
+  GLOBAL.DATA_FOLDER,
+  GLOBAL.LOGS_FOLDER,
+  GLOBAL.PRIVATE_FOLDER,
+  GLOBAL.USERS_FOLDER,
+];
+
+for (let i = 0; i < dataFolders.length; i++) {
+  if (!fs.existsSync(dataFolders[i])) {
+    try {
+      fs.mkdirSync(dataFolders[i]);
+      logger.info(
+        { "Data folder": dataFolders[i] },
+        "Creating missing data folder.",
+      );
+      dataFolderAdjusted = true;
+    } catch (err) {
+      error(
+        "Create missing data folders",
+        "Failed to create data folder.",
+        { "Data Folder": dataFolders[i] },
+        err,
+      );
+    }
+  }
+}
 
 app.listen(port, () => {
   console.log(`Backend listening on port ${port}`);
