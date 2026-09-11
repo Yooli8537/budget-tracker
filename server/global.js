f391cc2e2bbc7a897100b279260ce3c5a06407f5
Yooli8537 <Yooli8537@gmx.ch>
2026-09-11 11:34:19 +0200

    New global variables

--- server/global.js
+++ server/global.js
@@ -4,18 +4,21 @@ const path = require("path");
 // Paths
 // Folders
 const ROOT_FOLDER = path.join(__dirname, "../");
+const LOGS_FOLDER = path.join(ROOT_FOLDER, "logs");
+const SQL_FOLDER = path.join(ROOT_FOLDER, "sql");
 const SERVER_FOLDER = path.join(ROOT_FOLDER, "server");
 const ROUTES_FOLDER = path.join(SERVER_FOLDER, "routes");
 const DATA_FOLDER = path.join(ROOT_FOLDER, "data");
-const LOGS_FOLDER = path.join(ROOT_FOLDER, "logs");
 const PRIVATE_FOLDER = path.join(DATA_FOLDER, "private");
 const USERS_FOLDER = path.join(DATA_FOLDER, "users");
 // Server Routes
 const USERS_ROUTE = path.join(ROUTES_FOLDER, "users.routes.js");
-// Databases
+// Databases (files as well)
 const USERS_DATABASE = path.join(PRIVATE_FOLDER, "users.db");
 // Files
 const SERVER_SETTINGS = path.join(PRIVATE_FOLDER, "server.json");
+const USERS_DB_SQL = path.join(SQL_FOLDER, "users.sql");
+const USER_DATA_SQL = path.join(SQL_FOLDER, "perUserDatabase.sql");
 
 const GLOBAL = {
   PATHS: {
@@ -36,6 +39,8 @@ const GLOBAL = {
     },
     FILES: {
       SERVER_SETTINGS: SERVER_SETTINGS,
+      USERS_DB_SQL: USERS_DB_SQL,
+      USER_DATA_SQL: USER_DATA_SQL,
     },
   },
 };
