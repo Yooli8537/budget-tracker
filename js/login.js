83838bb83b459079d74c8ac2634ed9bfa949b500
Yooli8537 <Yooli8537@gmx.ch>
2026-09-11 14:54:47 +0200

    Added proper login page

--- js/login.js
+++ js/login.js
@@ -1,5 +1,8 @@
+const loginUsername = document.querySelector("#loginUsername");
+const loginPassword = document.querySelector("#loginPassword");
 const loginButton = document.querySelector("#loginButton");
 
 loginButton.addEventListener("click", () => {
-  window.location.href = "pages/overview.html";
+  console.log(loginUsername.value);
+  console.log(loginPassword.value);
 });
